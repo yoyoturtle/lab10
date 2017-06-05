5bee47e07034aa911b71e5c045ce9005dbd42ca1
yoyoturtle <yoyoturtle@users.noreply.github.com>
2017-06-05 19:22:32 +0800

    Add files via upload

---
+++ _2.ino
@@ -0,0 +1,27 @@
+#include<IRremote.h>
+const int irReceiverPin = 2;
+const int buttonPin = 4;
+int buttonState = 0;
+IRsend irsend;
+IRrecv irrecv(irReceiverPin);
+decode_results results;
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() 
+{
+
+   buttonState = digitalRead(buttonPin);
+   
+   if(buttonState == HIGH)
+   {
+    irsend.sendNEC(0x4FB48B7, 32);
+    Serial.print("X");
+    
+    }
+   
+}
